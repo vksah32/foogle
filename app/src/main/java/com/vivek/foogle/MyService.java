3b1f6d06ced51bfc402e23d3b39deaabfd6e69e3
vksah32 <vksah@colby.edu>
2015-10-03 14:52:48 -0400

    first commit

---
+++ app/src/main/java/com/vivek/foogle/MyService.java
@@ -0,0 +1,126 @@
+
+package com.vivek.foogle;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+public class MyService extends Service implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener {
+    private Location mLastLocation;
+    private GoogleApiClient mGoogleApiClient;
+    private boolean mConnected;
+    LocationRequest mLocationRequest;
+
+
+    public MyService() {
+        if (haveWifiConnection()) {
+            buildGoogleApiClient();
+            if (mGoogleApiClient != null) {
+                mGoogleApiClient.connect();
+            }
+        } else {
+            Toast.makeText(getApplicationContext(), "Connection failed!!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    public void onConnected(Bundle connectionHint) {
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        Log.i("LOCATION", "got it");
+        mConnected = true;
+        createLocationRequest();
+        if (mLocationRequest != null)
+            startLocationUpdates();
+
+
+
+    }
+
+
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Toast.makeText(getApplicationContext(), "Connection failed!!", Toast.LENGTH_SHORT).show();
+
+
+
+    }
+
+    private boolean haveWifiConnection() {
+        boolean haveConnectedWifi = false;
+
+//        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+//        NetworkInfo[] netInfo = cm.getAllNetworkInfo();
+//        for (NetworkInfo ni : netInfo) {
+//            if (ni.getTypeName().equalsIgnoreCase("WIFI"))
+//                if (ni.isConnected())
+//                    haveConnectedWifi = true;
+//        }
+        ConnectivityManager connManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo mWifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+
+        if (mWifi.isConnected()) {
+            haveConnectedWifi = true;
+        }
+        return haveConnectedWifi;
+    }
+
+
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(0);
+//        mLocationRequest.setFastestInterval(10);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+
+    protected void startLocationUpdates() {
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d("LOC", "location changed");
+        Toast.makeText(this, "Location changed", Toast.LENGTH_SHORT).show();
+        startLocationUpdates();
+
+
+    }
+
+
+
+}
