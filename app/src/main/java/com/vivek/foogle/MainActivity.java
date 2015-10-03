1f955ef8ebfeba613e18f343fa39daec8fe117c8
vksah32 <vksah@colby.edu>
2015-10-03 16:44:53 -0400

    location seems to be working

--- app/src/main/java/com/vivek/foogle/MainActivity.java
+++ app/src/main/java/com/vivek/foogle/MainActivity.java
@@ -1,17 +1,34 @@
 package com.vivek.foogle;
 
 import android.content.Intent;
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener {
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+    LocationRequest mLocationRequest;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        buildGoogleApiClient();
+        if (mGoogleApiClient != null) {
+                mGoogleApiClient.connect();
+            }
         setContentView(R.layout.activity_main);
         //start service
         // minimize window
@@ -44,7 +61,65 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         Toast.makeText(this, "lalallal",Toast.LENGTH_SHORT).show();
-        startService(new Intent(getBaseContext(), MyService.class));
 
     }
+
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+
+    public void onConnected(Bundle connectionHint) {
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (mLastLocation != null) {
+//            mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
+//            mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
+            Log.d("DEBUXX","HGCWGHCSHG");
+            Toast.makeText(this, String.valueOf(mLastLocation.getLatitude()), Toast.LENGTH_SHORT).show();
+        }
+        createLocationRequest();
+        boolean mRequestingLocationUpdates = true;
+        if (mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    protected void startLocationUpdates() {
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+    }
+
+    @Override
+
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+
+    public void onLocationChanged(Location location) {
+        mLastLocation = location;
+//        mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+//        updateUI();
+        Toast.makeText(MainActivity.this, "changed location", Toast.LENGTH_SHORT).show();
+    }
+
 }
