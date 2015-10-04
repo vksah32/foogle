efa6d850704fa946cc2aa832f34db54e702f3a16
Brenda Miao <bmiao@brendas-mbp.wireless.dartmouth.edu>
2015-10-03 23:14:53 -0400

    changes to notification

--- app/src/main/java/com/vivek/foogle/MainActivity.java
+++ app/src/main/java/com/vivek/foogle/MainActivity.java
@@ -6,6 +6,8 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.renderscript.RenderScript;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,37 +15,68 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private NotificationCompat.Builder mBuilder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //start service
-        createNotification(this, "Lous");
+
+        mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle("Foogle")
+                        .setContentText("New Menu received!")
+                        .setAutoCancel(true)
+                        .setPriority(Notification.PRIORITY_HIGH)
+                        .setCategory(Notification.CATEGORY_RECOMMENDATION);
+
         // minimize window
+        createNotification(this, "Lous");
 
     }
 
     public void createNotification(Context context, String restaurant) {
-        NotificationManager notificationManager = (NotificationManager) context
-                .getSystemService(Context.NOTIFICATION_SERVICE);
-        Notification notification = new Notification(R.mipmap.ic_launcher,
-                "Message received", System.currentTimeMillis());
-        // Hide the notification after its selected
-        notification.flags |= Notification.FLAG_AUTO_CANCEL;
+//        NotificationManager notificationManager = (NotificationManager) context
+//                .getSystemService(Context.NOTIFICATION_SERVICE);
+//        Notification notification = new Notification(R.mipmap.ic_launcher,
+//                "New Menu", System.currentTimeMillis());
+//        // Hide the notification after its selected
+//        notification.flags |= Notification.FLAG_AUTO_CANCEL;
+//
+//        //adding LED lights to notification
+//        notification.defaults |= Notification.DEFAULT_LIGHTS;
+//
+//        Intent intent = new Intent(this,
+//                Test.class);
+//        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0,
+//                intent, 0);
+//        notification.setLatestEventInfo(context, "Foogle",
+//                "New menu received", pendingIntent);
+//        notificationManager.notify(0, notification);
+        Intent resultIntent = new Intent(this, Test.class);
+// Because clicking the notification opens a new ("special") activity, there's
+// no need to create an artificial back stack.
+        PendingIntent resultPendingIntent =
+                PendingIntent.getActivity(
+                        this,
+                        0,
+                        resultIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
 
-        //adding LED lights to notification
-        notification.defaults |= Notification.DEFAULT_LIGHTS;
-
-        Intent intent = new Intent("android.intent.action.VIEW",
-                Uri.parse("http://my.example.com/"));
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0,
-                intent, 0);
-        notification.setLatestEventInfo(context, "Message",
-                "New message received", pendingIntent);
-        notificationManager.notify(0, notification);
-
+        mBuilder.setContentIntent(resultPendingIntent);
+        int mNotificationId = 001;
+// Gets an instance of the NotificationManager service
+        NotificationManager mNotifyMgr =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+// Builds the notification and issues it.
+        mNotifyMgr.notify(mNotificationId, mBuilder.build());
     }
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -71,7 +104,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         Toast.makeText(this, "lalallal",Toast.LENGTH_SHORT).show();
-        startService(new Intent(getBaseContext(), MyService.class));
 
     }
 }
